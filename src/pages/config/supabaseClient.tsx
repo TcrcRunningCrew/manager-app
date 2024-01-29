02c4691ae6a75b68490b10645b5769e434ec68f1
WHS95 <tjdngur22@gmail.com>
2024-01-29 23:53:35 +0900

    ✨ 기존 기능 추가

--- src/pages/config/supabaseClient.tsx
+++ src/pages/config/supabaseClient.tsx
@@ -1,11 +1,16 @@
-// // supabaseClient.js
-// import { createClient } from "@supabase/supabase-js";
+// supabaseClient.js
+import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = (process.env.SUPABASE_URL | String)
-// const supabaseKey = process.env.SUPABASE_API_KEY
-// // console.log('supabaseUrl: ', supabaseUrl);
-// // console.log('supabaseKey: ', supabaseKey);
+// const supabaseUrl = process.env.SUPABASE_URL || '';
+// const supabaseKey = process.env.SUPABASE_API_KEY|| '';
 
-// const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = 'https://eynjlxtyxihkokeesrlj.supabase.co' || '';
+const supabaseKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5bmpseHR5eGloa29rZWVzcmxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2NzMyNzIsImV4cCI6MjAyMTI0OTI3Mn0.3ZjFnmezUcgt9LxbtgB6Ag4ZGbgb-N-o6VCbja0Kiyc'|| '';
+// SUPABASEURL=
+// SUPABASEAPIKEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5bmpseHR5eGloa29rZWVzcmxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2NzMyNzIsImV4cCI6MjAyMTI0OTI3Mn0.3ZjFnmezUcgt9LxbtgB6Ag4ZGbgb-N-o6VCbja0Kiyc
 
-// export default supabase;
+
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
