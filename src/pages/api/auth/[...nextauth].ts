63d609b22242a21c8bdf18a911be5e2532f4a528
jinhoin <hoin.jin@zep.us>
2024-01-30 22:41:50 +0900

    feat(auth) :sparkles: Set middleware Default login Required

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,21 +1,21 @@
-import NextAuth from "next-auth"
+import NextAuth, {NextAuthOptions} from "next-auth"
 import KakaoProvider from "next-auth/providers/kakao";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
+  secret: process.env.NEXT_AUTH_SECRET,
+  debug: true,
   // Configure one or more authentication providers
   providers: [
     KakaoProvider({
       clientId: process.env.KAKAO_CLIENT_ID || '',
       clientSecret: process.env.KAKAO_CLIENT_SECRET || ''
-
     }),
     GithubProvider({
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
     }),
-
   ]
-}
+} satisfies NextAuthOptions
 
 export default NextAuth(authOptions)
