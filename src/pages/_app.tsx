907ee83b41f20d0b53e727a0cda9d1031e59379f
WHS95 <tjdngur22@gmail.com>
2024-02-02 23:09:58 +0900

    ♻️   ts 문법개선

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
-import type {AppProps} from "next/app";
-import {SessionProvider} from "next-auth/react";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import Footer from "@/components/common/Footer";
 
-
-export default function App({Component,  pageProps: { session, ...pageProps }}: AppProps) {
-  return <SessionProvider session={session}><Component {...pageProps} /></SessionProvider>
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
-
-
-
