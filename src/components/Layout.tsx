4cdb377e452349b2dce434c8b9d0967faa4212a2
jinhoin <whe1915@gmail.com>
2024-04-01 00:45:59 +0900

    feat(home) WIP!!

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,9 +3,7 @@ import {PropsWithChildren} from "react";
 export const Layout = ({ children }: PropsWithChildren) => {
     return (
         <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
-            <main className="flex flex-1 justify-center items-center flex-col w-full">
                 {children}
-            </main>
         </div>
     );
 };
--- src/pages/401.tsx
+++ src/pages/401.tsx
@@ -1,30 +1,25 @@
-import {Button} from "../components/ui/button";
-import {IcKakaoIcon} from "../components/icons/IcKakao";
-import {Layout} from "../components/Layout";
+import { Button } from "../components/ui/button";
+import { IcKakaoIcon } from "../components/icons/IcKakao";
+import { Layout } from "../components/Layout";
 
 
 const NotAuthPage = () => {
-
     return (
         <Layout>
-            <div className={'size-full min-h-full bg-cover bg-center'} style={{
+            <div className={'w-[100vw] h-[102vh] min-h-full bg-cover bg-center items-center'} style={{
                 backgroundImage: `url('BackImages.png')`
             }}>
-                <div className={'text-white flex gap-2 flex-col'}>
-                <span className={'text-2xl'}>
-                Tan-Cheon Running Crew
-             s   </span>
-                    <span>
-                    TCRC
-                </span>
-                    <span>
-                    We Run Together
-                </span>
+                <div className={'flex justify-center items-center flex-col'}>
+                    <div className={'text-white size-full flex gap-2 flex-col'}>
+                        <span className={'text-2xl'}>Tan-Cheon Running Crews</span>
+                        <span>TCRC</span>
+                        <span>We Run Together</span>
+                    </div>
+                    <Button variant={'default'}>
+                        <IcKakaoIcon/>
+                        카카오톡으로 로그인
+                    </Button>
                 </div>
-                <Button variant={'default'}>
-                    <IcKakaoIcon/>
-                    카카오톡으로 로그인
-                </Button>
             </div>
         </Layout>)
 }
