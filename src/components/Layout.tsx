fa0cba56d7b618ad5a1dc68801f51d30b6742162
DESKTOP-8M77RRB\ggh16 <ggh163700@gmail.com>
2024-06-03 23:30:06 +0900

    수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,20 +5,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 export const Layout = ({ children }: PropsWithChildren) => {
-    const router = useRouter();
-    const { data: session, status } = useSession();
-
-    useEffect(() => {
-        if (status === "authenticated" && session.user && session.user.name && session.user.email) {
-            if (router.pathname === "/login") {
-                router.push("/main");
-                return;
-            }
-        } else {
-            router.push("/login");
-            return;
-        }
-    }, [session, status]);
+    
 
     return (
         <div className="m-h-min bg-gray-100 flex flex-col items-center">
